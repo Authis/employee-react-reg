7946e2c044cae4cf5482ba11cb1654fe3d3e2b76
Authis <authis.sundar@gmail.com>
2020-01-08 11:09:34 +0000

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -61,7 +61,7 @@ class dashboard extends Component {
         <MuiThemeProvider>
           <div>
             <Typography className="loginTitle" component="h2" variant="h3" gutterBottom>
-              Employee Registration
+              Employee Registration 123
             </Typography>
 
               <TextField
